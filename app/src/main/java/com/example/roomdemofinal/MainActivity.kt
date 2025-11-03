f1d1030400d524fd57b428c262cf54b72e0032d7
shadowblack24 <shadowblack24@mail.ru>
2025-11-03 22:43:05 +0700

    1) Initial setup

---
+++ app/src/main/java/com/example/roomdemofinal/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.roomdemofinal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.roomdemofinal.ui.theme.RoomDemoFinalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RoomDemoFinalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenSetup(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    // Пусто
+}
